24e8c4a2676116ec415b4f56a4e9e61c977072f1
Nilan Dinushka <nilandinushka5@gmail.com>
2023-10-31 10:35:10 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep11/pos/controller/ManageCustomerFormController.java
@@ -0,0 +1,58 @@
+package lk.ijse.dep11.pos.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.dep11.pos.tm.Customer;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class ManageCustomerFormController {
+    public AnchorPane root;
+    public JFXTextField txtCustomerId;
+    public JFXTextField txtCustomerName;
+    public JFXTextField txtCustomerAddress;
+    public JFXButton btnSave;
+    public JFXButton btnDelete;
+    public TableView<Customer> tblCustomers;
+
+    public void navigateToHome(MouseEvent mouseEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/MainForm.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        Platform.runLater(primaryStage::sizeToScene);
+    }
+
+    public void btnAddNew_OnAction(ActionEvent actionEvent) {
+        for (TextField textField : new TextField[]{txtCustomerId, txtCustomerName, txtCustomerAddress}) {
+            textField.clear();
+        }
+        tblCustomers.getSelectionModel().clearSelection();
+        txtCustomerId.requestFocus();
+
+
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
+
+    private boolean isValidData(){
+
+    }
+}
